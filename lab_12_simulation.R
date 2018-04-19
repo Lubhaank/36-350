ea68ca5d5ffc8809094ef058eac9ddc1e9467e61
Lubhaank <lubhaankumar@gmail.com>
2018-04-19 15:03:42 -0400

    2b rem

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -9,27 +9,7 @@ generate_data = function(n,p){
 }
 
 
-model_select = function(covariates, responses, cutoff)
-{
-  index = c() #storing response column index
-  df = data.frame(cov = covariates, res = responses)
-  model = lm(formula = res ~ cov.1 + cov.2 + cov.3 + cov.4 + cov.5 + cov.6, data = df)
-  summ = summary(model)
-  for (i in 23:28)
-  {
-    if (summ[[4]][i] <= cutoff) 
-    {
-      index = c(index, (i - 22)) #index of the column to keep
-    }
-  }
-  if (length(index) == 0)
-  {
-    return(index)
-  }
-  lm_new = lm(formula = responses ~ covariates[, index])
-  mod.sum = summary(lm_new)
-  return(mod.sum$coefficients[,4])
-}
+
 
 
 
