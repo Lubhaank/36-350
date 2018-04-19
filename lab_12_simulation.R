fc5ffec3feba1d8601f1f279fe6778fca3036db0
Lubhaank <lubhaankumar@gmail.com>
2018-04-19 12:39:12 -0400

    2a

---
+++ lab_12_simulation.R
@@ -0,0 +1,12 @@
+generate_data = function(n,p){
+  
+  mat = matrix( rnorm(n*p,mean=0,sd=1), n, p) 
+  vec = rnorm(n,mean=0,sd=1)
+  result = list(covariates = mat, responses = vec)
+  return(result)
+  
+  
+}
+
+
+
